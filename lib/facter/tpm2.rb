ac2e89150d886f946921f52f0c7003cb42da5151
Chris Tessmer <chris.tessmer@onyxpoint.com>
2018-06-14 20:57:11 +0000

    Update tpm2 fact

--- lib/facter/tpm2.rb
+++ lib/facter/tpm2.rb
@@ -4,26 +4,48 @@
 # configured to comminucate with the TPM
 Facter.add('tpm2') do
 
-  #### The confine below is intentionally unused.
+  #### NOTE: The confine below is intentionally commented out (see comments).
+  ####
+  #### It has been left here to explain why we're not using it, or t
+  ####
+  #### Background:
   ####
   #### The `:has_tpm` detection strategy used for TPM 1 is unreliable for TPM
-  #### 2.0: TCTI can be configured to talk over network sockets, so the
-  #### TPM device the system is using may not be local.
+  #### 2.0.  TCTI can be configured to talk over different transports (including
+  #### network sockets), so the TPM device the system is using may not be in a
+  #### local `/dev/tmp#` device.
   ####
   #### This makes it impossible to conclusively detect whether a system has TPM2.0
-  #### capabilities unless the tpm2-tools software is installed and properly
-  #### configured.
+  #### capabilities until the tpm2-tools suite is installed and configured.
   ####
-  #### Therefore, the :has_tpm fact is *not* used to confine TPM 2.0 facts, and
-  #### the :tpm2 fact returns information if it is available
+  #### See comments/discussion at:
   ####
-  ### confine :has_tpm => true
+  #### * https://github.com/pohly/intel-iot-refkit/commit/2fec96c0b129986c2657214dd68f44ef79615d3c
+  #### * https://github.com/tpm2-software/tpm2-tools/issues/604
+  ####
+  #### As a consequence:
+  ####
+  #### * The `:has_tpm` fact from the original `tpm` module is *not* used to
+  ####   confine TPM 2.0 facts.
+  #### * Instead, the `:tpm2` fact returns information--only if it is available.
+  #### * The `tpm_version` fact from the original `tpm` module *can* be used to
+  ####   confine the tpm2 fact code *not* to run (if we have confirmed that the
+  ####   system has a TPM 1.2 device, there is no sense in checking for TPM 2
+  ####
+  #### confine :has_tpm => true
+  ###
+  #### ^^^ NOTE: The confine above is intentionally commented out (see comments).
 
-  # Don't check for TPM 2 data if the host is *known* to have a TPM 1 device.
+
+  # Confine the TPM2 fact to systems that are known NOT to have a TPM 1 device.
   #
-  # Note: The block is anonymous because this check will be true it `:tpm_version` is Facter doesn't execute confine blocks for
-  # absent facts.
-
+  # It only makes sense to test for TPM2 info if either:
+  # - If a local TPM device has *not* been detected (TCTI may use network)
+  # - If a local TPM device has been detected, but is *not* TPM 1.2
+  #
+  # NOTE: This confine block must remain anonymous--a direct confine on
+  # `:tpm_version` will always short-circuit if the fact is absent. (Facter
+  # doesn't execute confine blocks for absent facts.)
   confine do
      value = Facter[:tpm_version]
      Facter.debug 'tpm2 confine'
