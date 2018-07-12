fc87190345da28dc0acca9619132240266eba04b
Chris Tessmer <chris.tessmer@onyxpoint.com>
2018-07-12 23:40:58 +0000

    Acceptance tests work!

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -15,7 +15,6 @@ unless ENV['BEAKER_provision'] == 'no'
   end
 end
 
-
 # Download (and unpack if tarball of) TPM2 RPMs in the rpm_staging_dir
 # supports URLs ending in *.rpm, #.tar.gz, *.tar, and *.tgz
 def download_rpm_tarball_on(hosts, rpm_staging_dir)
@@ -36,16 +35,37 @@ def download_rpm_tarball_on(hosts, rpm_staging_dir)
   end
 end
 
+# Install all `*.rpm` files in `rpm_staging_dir/`
 def install_rpms_staged_on(hosts,rpm_staging_dir)
   on hosts, "yum localinstall -y #{rpm_staging_dir}/*.rpm"
 end
 
+# If local .rpm files have been staged in an 'rpms/' directory at the top
+# level of the repository, upload them to the SUTs' RPM staging directories
 def upload_locally_staged_rpms_to(hosts, rpm_staging_dir)
-  # If .rpm files have been locally staged at the top level of the repository, upload them as well
   rpms = Dir['*.rpm'] + Dir[File.join('rpms','*.rpm')]
   rpms.each do |f|
     scp_to(hosts,f,rpm_staging_dir)
   end
+end
+
+# Implement any workarounds that are needed to get
+def implement_workarounds(hosts)
+  # workaround for dbus config file mismatch error:
+  #
+  # "dbus[562]: [system] Unable to reload configuration: Configuration file
+  #  needs one or more <listen> elements giving addresses"
+  on hosts, 'systemctl restart dbus'
+end
+
+def install_pre_suite_rpms(hosts)
+  download_rpms   = !ENV.fetch('BEAKER_download_pre_suite_rpms','yes') == 'no'
+  rpm_staging_dir = "/root/rpms.#{$$}"
+
+  on hosts, "mkdir -p #{rpm_staging_dir}"
+  download_rpm_tarball_on(hosts, rpm_staging_dir) unless download_rpms
+  upload_locally_staged_rpms_to(hosts, rpm_staging_dir)
+  install_rpms_staged_on(hosts, rpm_staging_dir)
 end
 
 # starts tpm2sim service on (hosts)
@@ -68,31 +88,14 @@ def config_abrmd_for_tpm2sim_on(hosts)
   create_remote_file hosts, '/etc/systemd/system/tpm2-abrmd.service.d/override.conf', extra_file
   on hosts, 'systemctl daemon-reload'
 
-  # workaround for dbus config file mismatch error:
-  #
-  # "dbus[562]: [system] Unable to reload configuration: Configuration file
-  #  needs one or more <listen> elements giving addresses"
-  on hosts, 'systemctl restart dbus'
-
   on hosts, 'systemctl list-unit-files | grep tpm2-abrmd ' \
     + '&& systemctl restart tpm2-abrmd ' \
     + %q[|| echo "tpm2-abrmd.service not restarted because it doesn't exist"]
 end
 
-def install_pre_suite_rpms(hosts)
-  download_rpms   = !ENV.fetch('BEAKER_download_pre_suite_rpms','yes') == 'no'
-  rpm_staging_dir = "/root/rpms.#{$$}"
-
-  on hosts, "mkdir -p #{rpm_staging_dir}"
-  download_rpm_tarball_on(hosts, rpm_staging_dir) unless download_rpms
-  upload_locally_staged_rpms_to(hosts, rpm_staging_dir)
-  install_rpms_staged_on(hosts, rpm_staging_dir)
-end
-
 # start the tpm2sim and override tpm2-abrmd's systemd config use it
 # assumes the tpm2sim has been installed on the hosts
-def install_tpm2_0_tools(hosts)
-  install_pre_suite_rpms(hosts)
+def configure_tpm2_0_tools(hosts)
   start_tpm2sim_on(hosts)
   config_abrmd_for_tpm2sim_on(hosts)
 end
@@ -125,8 +128,9 @@ RSpec.configure do |c|
       # add PKI keys
       copy_keydist_to(server)
 
+      implement_workarounds(hosts)
       install_pre_suite_rpms(hosts)
-      install_tpm2_0_tools(hosts)
+      configure_tpm2_0_tools(hosts)
     rescue StandardError, ScriptError => e
       if ENV['PRY']
         require 'pry'; binding.pry
