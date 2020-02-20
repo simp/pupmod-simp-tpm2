fc819bcb136a57e4e4f38cf04be2357080df518b
jeannegreulich <jeanne.greulich@onyxpoint.com>
2020-02-20 15:33:40 -0500

    cleanup a little

--- manifests/init.pp
+++ manifests/init.pp
@@ -26,7 +26,6 @@
 # @author SIMP Team https://simp-project.com
 #
 class tpm2 (
-  Boolean                                    $manage_tpm2_tools = true,
   String[1]                                  $package_ensure    = simplib::lookup('simp_options::package_ensure', {'default_value' => 'installed'}),
   Hash[String[1], Hash[String[1],String[1]]] $packages          = simplib::lookup('tpm2::packages'),
   String[1]                                  $tabrm_service     = 'tpm2-abrmd',
@@ -40,28 +39,8 @@ class tpm2 (
     notify{ "NOTICE: Host has a tpm1 device; skipping TPM2 resources from module '${module_name}'": }
   } else {
     include 'tpm2::install'
-
-    if $manage_tpm2_tools {
-      include 'tpm2::service'
-
-      Class[ 'tpm2::install' ] ~> Class[ 'tpm2::service' ]
-    }
-
-    if $tabrm_options {
-      $_cmd_opts = join($tabrm_options, ' ')
-      $_override = @("OVERRIDE")
-        # This file managed by Puppet
-        [Service]
-        ExecStart=
-        ExecStart=/usr/sbin/${tabrm_service} ${_cmd_opts}
-        | OVERRIDE
-
-      systemd::dropin_file { 'tabrm_service.conf':
-        unit    => "${tabrm_service}.service",
-        content => $_override,
-        notify  => Class[tpm2::service]
-      }
-    }
+    include 'tpm2::service'
+    Class[ 'tpm2::install' ] ~> Class[ 'tpm2::service' ]
 
     if $take_ownership {
       include 'tpm2::ownership'
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,24 @@
 # @summary A private class to ensure that the TABRM service is running
 class tpm2::service {
   assert_private()
+
+
+  if $tpm2::tabrm_options {
+    $_cmd_opts = join($tpm2::tabrm_options, ' ')
+    $_override = @("OVERRIDE")
+      # This file managed by Puppet
+      [Service]
+      ExecStart=
+      ExecStart=/usr/sbin/${tpm2::tabrm_service} ${_cmd_opts}
+      | OVERRIDE
+
+    systemd::dropin_file { 'tabrm_service.conf':
+      unit    => "${tpm2::tabrm_service}.service",
+      content => $_override,
+      notify  => Service["#{$tpm2::tabrm_service}"]
+    }
+  }
+
   service{ $tpm2::tabrm_service:
     ensure => running,
     enable =>  true,
